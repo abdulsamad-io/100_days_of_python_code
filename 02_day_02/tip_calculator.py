396745ecf653ed076f9e359c0bcd4e054e577cc5
abdulsamad <abdulsamadayobami@gmail.com>
2026-06-04 01:00:23 +0200

    feat:adding restaurant tip calculator

---
+++ 02_day_02/tip_calculator.py
@@ -0,0 +1,15 @@
+# This program calculates the amount of tip to be paid and what share does each diner pay
+
+no_of_people = int(input("How many people will share the bill: "))
+
+net_bill = int(input("How much does the food cost total: EUR"))
+
+tip_percentage = int(input("How many percent (%) tip are you willing to give: "))
+
+total_bill = ((tip_percentage/100)+1) * net_bill
+
+per_person_bill = round(total_bill / no_of_people,3)
+
+print ("****** Thank you for dining with us ******")
+
+print (f"Your total bill is EUR {total_bill} and each person would pay EUR {per_person_bill}")
