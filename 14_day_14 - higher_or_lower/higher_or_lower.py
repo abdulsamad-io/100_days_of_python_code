57823d0c64bf5e6e7ae3be3a881cd200616b58fc
Abdulsamad Kazeem <abdulsamadayobami@gmail.com>
2026-08-10 11:56:50 +0200

    feat:updating 14_day_14 higher or lower_outer loop

--- 14_day_14 - higher_or_lower/higher_or_lower.py
+++ 14_day_14 - higher_or_lower/higher_or_lower.py
@@ -1,57 +1,64 @@
 import random
+from players import players as original_players
 
-players = {
-    "Cristiano Ronaldo": 677,
-    "Lionel Messi": 514,
-    "Neymar Jr": 242,
-    "Kylian Mbappé": 136,
-    "Erling Haaland": 75,
-}
+def new_game():
+    players = dict(original_players)
+    char_1 = random.choice(list(players.items()))
+    players.pop(char_1[0])
+    return players, char_1
 
-char_1 = random.choice(list(players.items()))
-players.pop(char_1[0])
-
-char_2 = ()
-higher = ()
-
-def play_game():
-    global char_2
-    global higher
+def play_round(players, char_1):
     char_2 = random.choice(list(players.items()))
     players.pop(char_2[0])
-    def compare_char(character_1, character_2):
-        if character_1[1] > character_2[1]:
-            return character_1
-        else:
-            return character_2
-    higher = compare_char(char_1, char_2)
 
-def compare_player_answer(answer):
-    global game_not_over
-    global char_1
-    chosen = char_1 if answer == "a" else char_2
+    if char_1[1] > char_2[1]:
+        higher = char_1
+    else:
+        higher = char_2
+
+    return char_2, higher
+
+def compare_player_answer(answer, char_1, char_2, higher):
+    if answer == "a":
+        chosen = char_1
+    else:
+        chosen = char_2
+
     if chosen[0] == higher[0]:
-        char_1 = higher
-        return "Correct"
+        return True
     else:
-        game_not_over = False
-        return "Wrong"
+        return False
 
-play_game()
-game_not_over = True
-score = 0
+keep_playing = True
 
-while game_not_over:
-    user_choice = input(f'Type a for {char_1[0]} or b for {char_2[0]}: ')
-    result = compare_player_answer(user_choice)
-    print(result)
+while keep_playing:
+    players, char_1 = new_game()
+    char_2, higher = play_round(players, char_1)
+    game_not_over = True
+    score = 0
 
-    if result == "Correct":
-        score += 1
-        if not players:
-            print(f"You've guessed every player! Final score: {score}")
-            game_not_over = False
+    while game_not_over:
+        user_choice = input(f'Type a for {char_1[0]} or b for {char_2[0]}: ')
+        is_correct = compare_player_answer(user_choice, char_1, char_2, higher)
+
+        if is_correct == True:
+            score = score + 1
+            print("Correct!")
+            char_1 = higher
+
+            if len(players) == 0:
+                print(f"You've guessed every player! Final score: {score}")
+                game_not_over = False
+            else:
+                char_2, higher = play_round(players, char_1)
         else:
-            play_game()  # only set up the next round if players are left
+            print(f"Wrong! Game over! Final score: {score}")
+            game_not_over = False
+
+    continue_play = input("Do you want to play again? (y/n): ")
+    if continue_play == "y":
+        keep_playing = True
     else:
-        print(f"Game over! Final score: {score}")
+        keep_playing = False
+
+print("bye")
