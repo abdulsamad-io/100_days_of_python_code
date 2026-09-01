874b02193f361edcae16ee891729d3b1d610f1c8
Abdulsamad Kazeem <akazeem@schubergphilis.com>
2026-09-01 16:14:28 +0200

    solving 02

--- 00_claude_code_challenges/topic_01_strings_and_variables/02_initials.py
+++ 00_claude_code_challenges/topic_01_strings_and_variables/02_initials.py
@@ -35,18 +35,29 @@ you have done it properly.
 """
 
 # --- write your code below this line ---
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+              'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-']
 
 user_names = []
-
 enter_name = True
 
 while enter_name:
-    user_name = input('Give me your name(s): ').lower()
-    if user_name != "":
-        user_names.append(user_name)
+    user_name = input('Give me your name, one at a time: ').lower()
+
+    error_chars = []
+    for char in user_name:
+        if char not in characters:
+            error_chars.append(char)
+
+    if error_chars:
+        print(f'The following characters are invalid, please try again: {error_chars}')
     else:
-        print('Please enter a valid name.')
+        user_names.append(user_name)
+
     continue_user_input = input('Do you want to continue with another name? (y/n): ').lower()
     if continue_user_input == 'n':
         enter_name = False
+
 print(user_names)
+
+initials = [name.split()[0][0].upper() for name in user_names]
